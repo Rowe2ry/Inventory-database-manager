158b65637fb14e93c46021a9a7bf303e229a6b31
Chris Rowe <rowe2ry@yahoo.com>
2022-04-26 22:30:28 -0400

    Added all 4 model files.

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const express = require('express'); // to create api HTTP methods
+const routes = require('./routes'); // to route the URL to the HTTP requests
+const sequelize = require('./config/connection') // to access ad interface with the database
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express(); // initialize the applicaiton
+const PORT = process.env.PORT || 3001; // default 3001 or whatever is available
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // add the ability to work with json files
+app.use(express.urlencoded({ extended: true })); // add the ability to pull paramters from user supplied URLs
 
+// make the routes work
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force : false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
