9db12b05aec30d6b9c739e73a471d1abb6e347fc
Chris Rowe <rowe2ry@yahoo.com>
2022-04-26 23:35:34 -0400

    Added GET request logi to the tag-routes.js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagsList = await Tag.findAll( {
+      include: [ { model: Product } , { model: ProductTag } ]
+    });
+    res.status(200).json(tagsList);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const oneTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product } , { model: ProductTag } ]
+    });
+    res.status(200).json(oneTag);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
 router.post('/', (req, res) => {
